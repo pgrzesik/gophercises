27fdebbe1c45cf83f9343953095c1fd031a8f0d2
Piotr Grzesik <pj.grzesik@gmail.com>
2018-05-21 18:10:55 +0200

    Handle error while opening file

--- quiz/main.go
+++ quiz/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of question,answer")
 	flag.Parse()
 	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open CSV file: %s\n", *csvFilename)
+		os.Exit(1)
+	}
+
+	_ = file
 }
